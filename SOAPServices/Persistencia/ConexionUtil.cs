c4b3e9ffe2dfae001113ef5415caa7f93cd557a3
soportejvm <soportejvm@gmail.com>
2016-02-10 22:56:39 -0500

    ...

--- SOAPServices/Persistencia/ConexionUtil.cs
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -10,7 +10,7 @@ namespace SOAPServices.Persistencia
         public static string ObtenerCadena()
         {
             
-            // return "Data Source=(local);Initial Catalog=DB_Usuarios;Integrated Security=SSPI;";
+             //return "Data Source=(local);Initial Catalog=DB_Usuarios;Integrated Security=SSPI;";
             return "Data Source=5d4d5fd8-c1fc-4551-a083-a5a600143ec9.sqlserver.sequelizer.com;Initial Catalog=db5d4d5fd8c1fc4551a083a5a600143ec9;User Id=gioidjclvbhzbbmm;Password=X8KpPqhDKeuEuETy2m3QWaxfKhyNPMVrkriP6BG8ndScdJfur2cYkVPXHDLGYAW7";
         }
     }

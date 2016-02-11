151cec9e7722b5df1ba0ac4749d5f3804dfc9fb9
soportejvm <soportejvm@gmail.com>
2016-02-10 23:32:45 -0500

    ...hhhh

--- SOAPServices/Persistencia/ConexionUtil.cs
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -11,7 +11,7 @@ namespace SOAPServices.Persistencia
         {
             
              //return "Data Source=(local);Initial Catalog=DB_Usuarios;Integrated Security=SSPI;";
-            // 1er db online return "Data Source=5d4d5fd8-c1fc-4551-a083-a5a600143ec9.sqlserver.sequelizer.com;Initial Catalog=db5d4d5fd8c1fc4551a083a5a600143ec9;User Id=gioidjclvbhzbbmm;Password=X8KpPqhDKeuEuETy2m3QWaxfKhyNPMVrkriP6BG8ndScdJfur2cYkVPXHDLGYAW7";
+            //dsdsde 1er db online return "Data Source=5d4d5fd8-c1fc-4551-a083-a5a600143ec9.sqlserver.sequelizer.com;Initial Catalog=db5d4d5fd8c1fc4551a083a5a600143ec9;User Id=gioidjclvbhzbbmm;Password=X8KpPqhDKeuEuETy2m3QWaxfKhyNPMVrkriP6BG8ndScdJfur2cYkVPXHDLGYAW7";
             return "Data Source=77b6ca3e-6edc-48f0-8239-a5a90043d1b0.sqlserver.sequelizer.com;Initial Catalog=db77b6ca3e6edc48f08239a5a90043d1b0;User Id=hlvhrmqdqdkbaebi;Password=NKnhqBVreEiwXdQMmT4V8ARghuGA7q6GCkdcs7LWNHmitqrHAUZpqqtn3ga53utW";
         }
     }
